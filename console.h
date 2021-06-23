d0b335c986b241777206a66c5a8e353c12bec660
AndreMerendeira <andre.merendeira@gmail.com>
2021-06-23 12:56:13 +0100

    reorganized repo into folders

--- console.h
+++
@@ -1,9 +0,0 @@
-char cnsl_getchar();
-void cnsl_putchar(char byte);
-void cnsl_puts(char* s);
-void cnsl_printf(char* fmt, ...);
-int cnsl_getint();
-void cnsl_putint(int x);
-void cnsl_open(char *devname);
-void cnsl_close();
-void cnsl_perror (char * mesg);
